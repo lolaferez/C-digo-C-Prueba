6332add670765158f14d0ea1171335727914fde2
lolaferez <126658983+lolaferez@users.noreply.github.com>
2023-03-13 17:28:16 +0100

    Add files via upload

---
+++ Ejemplo C++.cpp
@@ -0,0 +1,29 @@
+/*Programa que genera 20 números enteros comprendidos entre el -12.5 y el 12.5. Además muestra el del valor más alto y el más bajo
+  Entrada 20 números enteros */
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+int main (void)
+{
+    double num, media, maximo=0, minimo=0;
+    int i;
+        for (i=1; i<20; i++)
+        {
+            num = -12.5 + rand() %25;
+
+             if (num>maximo)
+                {
+
+
+
+                jjjjmmkklffdfdfdfsd
+                cout<<num<<", ";
+        }
+        cout<<maximo;
+        return 0;
+}
